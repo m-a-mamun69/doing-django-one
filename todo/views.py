8e6761a2d27c03a18753e54aac0befe823f02421
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2024-07-31 18:31:51 +0600

    Templates and template inheritance also adding some Files

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("This My Home Page For this Projects")
+    return render(request, 'index.html')
 
 
 def register(request):
-    return HttpResponse("This My Register Page For this Projects")
+    return render(request, 'register.html')
 
 
 def my_login(request):
-    return HttpResponse("This My Login Page For this Projects")
+    return render(request, 'my-login.html')
